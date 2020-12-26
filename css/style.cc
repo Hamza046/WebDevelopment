8cda2a353a719256042f23f5b707a4ae69adfe6e
Hamza046 <hamzaaydin046@gmail.com>
2020-12-26 18:58:05 +0300

    version 1.0

---
+++ css/style.cc
@@ -0,0 +1,9 @@
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap');
+.logo{
+  font-family: 'Raleway', sans-serif;
+  font-size:2.5rem;
+  font-weight: bold;
+}
+.ust-kisim{
+  background-color:#e3fdfd;
+}
